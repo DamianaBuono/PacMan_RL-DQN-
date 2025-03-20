58e3d8c02bce4795c1048809da4bf3f3dacb1da9
DamianaBuono <damianabuono2000@gmail.com>
2025-03-20 09:59:03 +0100

    Modifica della rete neurale più profonda

--- reinforcementDQL.py
+++ reinforcementDQL.py
@@ -9,14 +9,17 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class DQN(nn.Module):
     def __init__(self, state_size, action_size):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(state_size, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, action_size)
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, action_size)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
-        return self.fc3(x)
+        x = torch.relu(self.fc3(x))
+        return self.fc4(x)
+
 
 class PrioritizedReplayBuffer:
     def __init__(self, capacity, alpha=0.6):
